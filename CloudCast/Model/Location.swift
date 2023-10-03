b9f37fdf3fc194d217df352f7c5a79f44b6b25d2
Ashley Acevedo <40500769+ashleyrennee@users.noreply.github.com>
2023-10-02 18:37:00 -0600

    Completed Project 4

---
+++ CloudCast/Model/Location.swift
@@ -0,0 +1,14 @@
+//
+//  Location.swift
+//  CloudCast
+//
+//  Created by Ashley Acevedo on 10/2/23.
+//
+
+import Foundation
+
+struct Location {
+  let name: String
+  let latitude: Double
+  let longitude: Double
+}
